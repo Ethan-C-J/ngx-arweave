4f88c360daaa2d84b5f19413a7bfe8a72c44b45b
Ethan-C-J <ecluculent@gmail.com>
2023-01-01 09:35:17 +1000

    Implemented schemas for blocks and info endpoints

--- projects/ngx-arweave/src/lib/schemas/BlockSchema.ts
+++ projects/ngx-arweave/src/lib/schemas/BlockSchema.ts
@@ -1,29 +1,37 @@
-export interface BlockSchema {
-    status: number;
-    nonce?: string;
-    previousBlock?: string;
-    timestamp?: number;
-    lastRetarget?: number;
-    diff?: string;
-    height?: number;
-    hash?: string;
-    txRoot?: string;
-    txTree?: Array<string>;
-    walletList?: string;
-    rewardAddr?: string;
-    tags?: Array<string>;
-    rewardPool?: number;
-    weaveSize?: number;
-    blockSize?: number;
-    cumulativeDiff?: string;
-    hashListMerkle?: string;
-    poa?: __Poa;
-    error?: string;
+import { Tag } from "arweave/node/lib/transaction";
+export interface BlockResponse {
+    "nonce": string;
+    "previous_block": string;
+    "timestamp": number;
+    "last_retarget": number;
+    "diff": string;
+    "height": number;
+    "hash": string;
+    "indep_hash": string;
+    "txs": Array<string>;
+    "tx_root": string;
+    "tx_tree": Array<string>;
+    "wallet_list": string;
+    "reward_addr": string;
+    "tags": Array<Tag>;
+    "reward_pool": number;
+    "weave_size": number;
+    "block_size": number;
+    "cumulative_diff": string;
+    "hash_list_merkle": string;
+    "poa": {
+        "option": string;
+        "tx_path": string;
+        "data_path": string;
+        "chunk": string;
+    };
 }
 
-export interface __Poa {
-    option: string;
-    txPath: string;
-    dataPath: string;
-    chunk: string;
+export interface BlockError {
+    status: number;
+    error: string;
+}
+
+export function isBlockResponse(data: any): data is BlockResponse { 
+    return data.hasOwnProperty("previous_block")
 }
